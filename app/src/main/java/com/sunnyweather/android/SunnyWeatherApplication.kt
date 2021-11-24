9e098689a6af206f0731b760dbfb7294121ce431
luanf <luanf@mxnavi.com>
2021-11-24 17:32:41 +0800

    202111241732

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,21 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        //TODO
+        const val TOKEN = "填入你申请到的令牌值"
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
